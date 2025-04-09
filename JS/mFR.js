dce89812d150b8c1c71346b5fc108a50fd8ceea9
Dibendu Ghosal <128916258+dibendughosal@users.noreply.github.com>
2025-04-10 00:02:52 +0530

    Map, Reduce, filter Done

---
+++ JS/mFR.js
@@ -0,0 +1,14 @@
+let arr = [12, 15, 17, 19, 23];
+let arr2 = arr.map((val) => val**2);
+console.log(arr2);
+
+let array1 = [15,18,16,12,7];
+let array2 = array1.filter(val => val > 15);
+console.log(array2);
+
+let ary1 = [13, 17,15 , 18, 2];
+let ary2 = ary1.reduce((accumalator, currentValue) => accumalator + currentValue,0);
+console.log(ary2);
+
+
+
