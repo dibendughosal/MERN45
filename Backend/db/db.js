bda33f0107775265e1aa069bf8eb4d3748a6580e
Dibendu Ghosal <128916258+dibendughosal@users.noreply.github.com>
2025-04-30 22:24:40 +0530

    Product

--- Backend/db/db.js
+++ Backend/db/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-
+const app = require('express');
 const connectToDB = async () => {
     try{
         await mongoose.connect("mongodb://localhost:27017/ECOMZ");
--- Backend/index.js
+++ Backend/index.js
@@ -1,10 +1,9 @@
 const express = require('express');
 const connectToDB = require('./db/db');
-
 const app = express();
 
+app.use("/", userRouter);
 connectToDB();
-
 app.listen(7777, () => {
     console.log("Server is Running at 7777")
 })
